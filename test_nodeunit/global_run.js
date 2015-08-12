12a3fe92859cb4acc28c2a66ce87a893bbdb0353
Fabian Schmid <fabian.schmid@wca.at>
2015-08-12 19:01:05 +0200

    fix build integration

--- test_nodeunit/global_run.js
+++ test_nodeunit/global_run.js
@@ -1,4 +1,4 @@
-/*! grunt-createunit - v0.0.2 - 2015-08-10
+/*! grunt-createunit - v0.0.3 - 2015-08-12
 * https://github.com/fabian-schmid/grunt-createunit
 * Copyright (c) 2015 Fabian schmid; Licensed MIT */
 
