ae9c0c1df13571469e923faa7501bc0320c55df5
Fabian Schmid <fabian.schmid@wca.at>
2015-10-17 20:34:44 +0200

    update devDependencies

--- test_nodeunit/global_run.js
+++ test_nodeunit/global_run.js
@@ -1,7 +1,3 @@
-/*! grunt-createunit - v0.1.0 - 2015-08-21
-* https://github.com/fabian-schmid/grunt-createunit
-* Copyright (c) 2015 Fabian schmid; Licensed MIT */
-
 "use strict;"
 
 require("../src/test");
